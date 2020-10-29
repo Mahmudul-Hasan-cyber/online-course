d820e559eb2896d5bed14da3295a1a7eccc295e5
SharifHasan-786 <sharif016693@gmail.com>
2020-10-29 09:16:25 +0800

    online course done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+import './Header.css'
+
+const header = () => {
+    return (
+        <div className='header'>
+            <img src={logo} alt="" srcset=""/>
+            <nav>
+                <a href="/buy">All Courses</a>
+                <a href="/review">Order </a>
+                <a href="/manage">Manage Inventory</a>
+            </nav>
+        </div>
+    );
+};
+
+export default header;
